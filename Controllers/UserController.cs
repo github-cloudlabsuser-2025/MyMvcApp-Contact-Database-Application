c9ad40304297b989100ec39e6914ccde6afbee17
Guribe <github_cloudlabsuser_2025@cloudlabsaiuser.com>
2024-11-20 18:32:17 +0000

    Add deployment parameters and configuration files for Azure App Service

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MyMvcApp.Models;
 
-namespace MyMvcApp.Controllers;
+namespace MyMvcApp.Controllers{
 
 public class UserController : Controller
 {
@@ -17,6 +17,10 @@ public class UserController : Controller
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
+          var existe = userlist.FirstOrDefault(u => u.Id == id);
+          if(existe == null) {
+                NotFound();
+            }
            return View("~/Views/User/Details.cshtml", userlist[id]); // Ensure the method returns a view with the user details
         }
 
@@ -29,41 +33,95 @@ public class UserController : Controller
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
-        {
-          return View("~/Views/User/Create.cshtml", user); // Ensure the method returns a view to create a new user
+        {  
+            var existe = userlist.FirstOrDefault(u => u.Id == user.Id);
+            if(existe == null) {
+                user.Id = userlist.Count;
+                userlist.Add(user);
+            }
+           
+           return RedirectToAction("Index");
+           //return View("~/Views/User/Index.cshtml", userlist); // Ensure the method returns a view to create a new user
         }
 
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
+                // Retrieve the user by ID from the user list
+    var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+    // If the user is not found, return HttpNotFound
+    if (existingUser == null)
+    {
+        return NotFound();
+    }
            return View("~/Views/User/Edit.cshtml", userlist[id]); // Ensure the method returns a view to edit a user}
         }
-        // POST: User/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, User user)
-        {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
-            // Implement the Edit method (POST) here
+ // POST: User/Edit/5
+[HttpPost]
+public ActionResult Edit(int id, User user)
+{
+    // Retrieve the user by ID from the user list
+    var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+    // If the user is not found, return HttpNotFound
+    if (existingUser == null)
+    {
+        return NotFound();
+    }
 
-            return View("~/Views/User/Edit.cshtml", user); // Ensure the method returns a view to edit a user
-        }
+    // Update the user's information with the data from the form submission
+    if (ModelState.IsValid)
+    {
+        existingUser.Name = user.Name;
+        existingUser.Email = user.Email;
+        
+        // Save the changes (assuming userList is a database context or similar)
+        // dbContext.SaveChanges(); // Uncomment if using a database context
 
-        // GET: User/Delete/5
-        public ActionResult Delete(int id)
-        {
-            // Implement the Delete method (GET) here
-            return View("~/Views/User/Delete.cshtml", userlist[id]); // Ensure the method returns a view to delete a user
-        }
+        // Redirect to the Index action if successful
+        return RedirectToAction("Index");
+    }
 
-        // POST: User/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            // Implement the Delete method (POST) here
-            return View("~/Views/User/Delete.cshtml", userlist[id]); // Ensure the method returns a view to delete a user
-        }
+    // If an error occurs, return the Edit view with validation errors
+    return View("~/Views/User/Edit.cshtml", user);
+}
+
+// GET: User/Delete/5
+public ActionResult Delete(int id)
+{
+    // Retrieve the user by ID from the user list
+    var user = userlist.FirstOrDefault(u => u.Id == id);
+    
+    // If the user is not found, return HttpNotFound
+    if (user == null)
+    {
+        return NotFound();
+    }
+
+    // Return the Delete view with the user to be deleted
+    return View("~/Views/User/Delete.cshtml", user);
+}
+
+// POST: User/Delete/5
+[HttpPost]
+public ActionResult Delete(int id, IFormCollection collection)
+{
+    // Retrieve the user by ID from the user list
+    var user = userlist.FirstOrDefault(u => u.Id == id);
+    
+    // If the user is not found, return HttpNotFound
+    if (user == null)
+    {
+        return NotFound();
+    }
+
+    // Remove the user from the user list
+    userlist.Remove(user);
+    
+    // Save the changes (assuming userList is a database context or similar)
+    // dbContext.SaveChanges(); // Uncomment if using a database context
+
+    // Redirect to the Index action if successful
+    return RedirectToAction("Index");
+}
+}
 }
--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01f751434578b90b46ac6a982a5e25b6438a06a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cf059e068ec27c44b7a57eb2aae936395ea9635")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
